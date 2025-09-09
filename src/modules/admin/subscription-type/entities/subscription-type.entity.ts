ee03a379c375256aeb98e0e1bf5fd209929e539e
Nirob844 <nirobhossainalip@gmail.com>
2025-09-09 16:27:56 +0600

    subcription type

---
+++ src/modules/admin/subscription-type/entities/subscription-type.entity.ts
@@ -0,0 +1,12 @@
+export class SubscriptionType {
+    id: string;
+    type: string;
+    games: number;
+    questions: number;
+    players: number;
+    price: number;
+    status: string;
+    language_id: string;
+    created_at: Date;
+    updated_at: Date;
+}
--- src/modules/application/application.module.ts
+++ src/modules/application/application.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { NotificationModule } from './notification/notification.module';
 import { ContactModule } from './contact/contact.module';
 import { FaqModule } from './faq/faq.module';
+import { GameModule } from './game/game.module';
+import { GamePlayerModule } from './game-player/game-player.module';
 
 @Module({
-  imports: [NotificationModule, ContactModule, FaqModule],
+  imports: [NotificationModule,
+    ContactModule,
+    FaqModule,
+    GameModule,
+    GamePlayerModule,],
 })
-export class ApplicationModule {}
+export class ApplicationModule { }
